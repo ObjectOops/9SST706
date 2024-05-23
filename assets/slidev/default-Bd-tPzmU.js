75f0438434edf658941a63cf10b5466eea525431
ObjectOops <92550885+ObjectOops@users.noreply.github.com>
2024-05-23 19:39:15 +0000

    Fix subroute issue again.

---
+++ assets/slidev/default-Bd-tPzmU.js
@@ -0,0 +1,1 @@
+import{o,b as s,f as t}from"../modules/vue-C0T5wuS6.js";import{_ as a}from"../index-B5tKaCiw.js";const c={},l={class:"slidev-layout default"};function r(e,n){return o(),s("div",l,[t(e.$slots,"default")])}const f=a(c,[["render",r],["__file","/workspaces/9SST706/node_modules/@slidev/client/layouts/default.vue"]]);export{f as I};
